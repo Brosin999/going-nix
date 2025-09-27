5e3276754add3cffb8be9444ed3b432fe3ff9c83
brosin <brosinnine@gmail.com>
2025-09-27 15:48:31 +0100

    screenshare

--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -6,4 +6,22 @@
 
   programs.niri.enable = true;
   programs.niri.package = pkgs.niri;
+
+  # Desktop portals for screen sharing
+  xdg.portal = {
+    enable = true;
+    config.niri = {
+      default = ["gnome" "gtk"];
+      "org.freedesktop.impl.portal.Access" = "gtk";
+      "org.freedesktop.impl.portal.Notification" = "gtk";
+      "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
+      "org.freedesktop.impl.portal.FileChooser" = "gtk";
+      "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+      "org.freedesktop.portal.ScreenCast" = "gnome";
+    };
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+    ];
+  };
 }
